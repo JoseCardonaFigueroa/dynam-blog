ad89a4e666f439871f6d41c3232979c86be32a15
José Cardona Figueroa <cardona5d@gmail.com>
2016-01-02 17:17:46 -0600

    Gave eventhandler a better structure

--- app/e_handler.js
+++ app/e_handler.js
@@ -1,27 +1,21 @@
-// (function e_handler() {
-(  function registerEventsFunc () {
-    var menuMobileBtn = document.querySelector("#btn-submenu-container"),
-    menuMobileContainer = document.querySelector("#main-nav"),
-    bodyTag = document.getElementsByTagName('body')[0],
-    target = null;
+(function registerEventsFunc () {
+  var menuMobileBtn = document.querySelector("#btn-submenu-container"),
+  menuMobileContainer = document.querySelector("#main-nav"),
+  bodyTag = document.getElementsByTagName('body')[0],
+  target = null;
 
-    menuMobileBtn.addEventListener("touchstart", function (event) {
-      // target = event.target.localName === 'span' ? event.target.parentElement : event.target
-      target = event.target;
-      console.log(target.classList.length);
-      // console.log(target);
+  menuMobileBtn.addEventListener("touchstart", function (event) {
+    target = event.target;
+    console.log(target.classList.length);
 
-      if (target.classList.length === 0) {
-        bodyTag.classList.add('no-scroll');
-        menuMobileContainer.classList.add('open');
-        target.classList.add('open');
-        // console.log(target.classList.length);
-      }else {
-        bodyTag.classList.remove('no-scroll');
-        menuMobileContainer.classList.remove('open');
-        target.classList.remove('open');
-      };
-    }, false);
-  })();
-  // registerEventsFunc();
-// })();
+    if (target.classList.length === 0) {
+      bodyTag.classList.add('no-scroll');
+      menuMobileContainer.classList.add('open');
+      target.classList.add('open');
+    }else {
+      bodyTag.classList.remove('no-scroll');
+      menuMobileContainer.classList.remove('open');
+      target.classList.remove('open');
+    };
+  }, false);
+})();
