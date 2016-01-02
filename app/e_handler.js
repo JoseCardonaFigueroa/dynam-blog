f0e2ec40ef3b96f787b6ab848a41a4dc919d712f
José Cardona Figueroa <cardona5d@gmail.com>
2016-01-02 17:00:27 -0600

    Added responsive button and menu

---
+++ app/e_handler.js
@@ -0,0 +1,27 @@
+// (function e_handler() {
+(  function registerEventsFunc () {
+    var menuMobileBtn = document.querySelector("#btn-submenu-container"),
+    menuMobileContainer = document.querySelector("#main-nav"),
+    bodyTag = document.getElementsByTagName('body')[0],
+    target = null;
+
+    menuMobileBtn.addEventListener("touchstart", function (event) {
+      // target = event.target.localName === 'span' ? event.target.parentElement : event.target
+      target = event.target;
+      console.log(target.classList.length);
+      // console.log(target);
+
+      if (target.classList.length === 0) {
+        bodyTag.classList.add('no-scroll');
+        menuMobileContainer.classList.add('open');
+        target.classList.add('open');
+        // console.log(target.classList.length);
+      }else {
+        bodyTag.classList.remove('no-scroll');
+        menuMobileContainer.classList.remove('open');
+        target.classList.remove('open');
+      };
+    }, false);
+  })();
+  // registerEventsFunc();
+// })();
